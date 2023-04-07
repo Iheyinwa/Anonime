a8481703c278b6e4730aff791901f73f0302ae95
Iheyinwa <caruogu@outlook.com>
2023-04-06 19:31:10 -0700

    landing page for desktop view

---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	/* margin: 0px 83px; */
+`;
+export const StyledNav = styled.nav`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`;
+export const StyledH3 = styled.h3`
+	font-weight: bold;
+	font-size: 36px;
+	line-height: 32px;
+`;
+export const StyledUl = styled.ul`
+	list-style: none;
+	display: flex;
+`;
+export const StyledLi = styled.li`padding: 10px;`;
+
+export const StyledInput = styled.input`
+	background-color: #374151;
+	border: 1px solid #374151;
+	border-radius: 40px;
+	width: 374.58px;
+	padding: 10px;
+	text-align: left;
+	color: #868686;
+	font-weight: 400;
+`;
