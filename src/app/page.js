1b6d48382d50c18fdb249009850d240e43bedc7f
Iheyinwa <caruogu@outlook.com>
2023-04-06 19:36:48 -0700

    removed metadata from CSC

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,6 @@
 import Explore from '@/components/Explore/Explorecomponents';
 import Release from '@/components/Release/Releasecomponents';
 
-export const metadata = {
-	title: 'Anonime'
-};
-
 export default function Home() {
 	return (
 		<main>
